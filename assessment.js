ca02e2c1d5654f9db80572dbbf9086c0874ca488
erniesaki <renzsaquilayan@hotmail.com>
2018-04-20 15:46:55 +0100

    first commit

---
+++ assessment.js
@@ -0,0 +1,29 @@
+
+function isPrime(number){
+	let num1 = parseInt(document.getElementById(input1));
+	if (Math.isPrime(num1)){
+		window.alert("its a prime number");
+	}
+	else {
+		window.alert("its not a prime number");
+	}
+}
+  
+function amountToCoins(amount, coins){
+
+}
+
+function uniqueLetters(string){
+
+}
+
+function factorial(number){
+	let num3 = parseInt(document.getElementById(input1));
+	for(num3=>1; num3--){
+		num3
+	}
+}
+
+function fibonacci(number){
+
+}
